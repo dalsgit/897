bedfb2d33d1acd5decbf1c332071235a90927dae
Nirbaan Maken <Nirbaan Maken>
2017-11-15 17:26:56 -0500

    f

--- hw/rlab_11.R
+++ hw/rlab_11.R
@@ -105,7 +105,7 @@ boost.boston =gbm(medv~.,data=Boston [train ,], distribution=
 summary (boost.boston )
 par(mfrow =c(1,2))
 plot(boost.boston ,i="rm")
-plot(boost.boston ,i=" lstat")
+plot(boost.boston ,i="lstat")
 yhat.boost=predict (boost.boston ,newdata =Boston [-train ,],
                         n.trees =5000)
 mean(( yhat.boost -boston.test)^2)
@@ -115,3 +115,38 @@ boost.boston =gbm(medv~.,data=Boston [train ,], distribution=
 yhat.boost=predict (boost.boston ,newdata =Boston [-train ,],
                       n.trees =5000)
 mean(( yhat.boost -boston.test)^2)
+
+plot(boost.boston ,i="dis")
+set.seed(1)
+boost.boston =gbm(medv~.,data=Boston [train ,], distribution=
+                    "gaussian",
+                  n.trees=5000, interaction.depth=4, shrinkage=0.01,
+                  verbose =F)
+yhat.boost=predict (boost.boston ,newdata =Boston [-train ,],
+                    n.trees =5000)
+mean(( yhat.boost -boston.test)^2)
+
+set.seed(1)
+boost.boston =gbm(medv~.,data=Boston [train ,], distribution=
+                    "gaussian",
+                  n.trees=5000, interaction.depth=3, shrinkage=0.01,
+                  verbose =F)
+yhat.boost=predict (boost.boston ,newdata =Boston [-train ,],
+                    n.trees =5000)
+mean(( yhat.boost -boston.test)^2)
+
+
+q11data=readRDS("q11data.Rda")
+q11data$Y <- as.factor(q11data$Y)
+str(q11data)
+
+tree.q11 =tree(Y~., q11data)
+summary (tree.q11 )
+
+plot(tree.q11 )
+text(tree.q11 ,pretty =0)
+
+df_test<-data.frame(0, 14, 14, 0.1, 0.1, 0.1)
+names(df_test)<-c("Y", "X1", "X2", "X3", "X4", "X5")
+
+predict (tree.q11, df_test,type ="class")
